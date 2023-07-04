cb7eb8777971fd863e52b049cc57939e5b4599f1
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-07-04 17:52:43 +0800

    Springing and Damping

--- Assets/Scripts/LivelyCamera.cs
+++ Assets/Scripts/LivelyCamera.cs
@@ -4,10 +4,14 @@ public class LivelyCamera : MonoBehaviour
 {
     [SerializeField, Min(0f)]
     float
+        springStrength = 100f,
+        dampingStrength = 10f,
         jostleStrength = 40f,
         pushStrength = 1f;
 
-    Vector3 velocity;
+    Vector3 anchorPosition, velocity;
+
+    void Awake() => anchorPosition = transform.localPosition;
 
     public void JostleY() => velocity.y += jostleStrength;
 
@@ -19,6 +23,11 @@ public class LivelyCamera : MonoBehaviour
 
     void LateUpdate()
     {
+        // We implement the spring by using the current displacement of the camera scaled by the spring strength as acceleration.
+        // We also slow down movement via a negative acceleration equal to the current velocity scaled by the damping strength.
+        Vector3 displacement = anchorPosition - transform.localPosition;
+        Vector3 acceleration = springStrength * displacement - dampingStrength * velocity;
+        velocity += acceleration * Time.deltaTime;
         transform.localPosition += velocity * Time.deltaTime;
     }
 }
