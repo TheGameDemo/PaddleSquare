894601265553ad4710eb87154abfc405766f8d9f
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-07-04 18:10:04 +0800

    Delta Time Maximum

--- Assets/Scripts/LivelyCamera.cs
+++ Assets/Scripts/LivelyCamera.cs
@@ -7,7 +7,8 @@ public class LivelyCamera : MonoBehaviour
         springStrength = 100f,
         dampingStrength = 10f,
         jostleStrength = 40f,
-        pushStrength = 1f;
+        pushStrength = 1f,
+        maxDeltaTime = 1f / 60f;
 
     Vector3 anchorPosition, velocity;
 
@@ -21,13 +22,27 @@ public class LivelyCamera : MonoBehaviour
         velocity.z += pushStrength * impulse.y;
     }
 
+    // If the frame rate is too low the overshoot might end up exaggerating its momentum and it can go out of control,
+    // speeding up instead of slowing down.
+    // We can avoid it by enforcing a small time delta.
     void LateUpdate()
     {
         // We implement the spring by using the current displacement of the camera scaled by the spring strength as acceleration.
         // We also slow down movement via a negative acceleration equal to the current velocity scaled by the damping strength.
+        float dt = Time.deltaTime;
+        while (dt > maxDeltaTime)
+        {
+            TimeStep(maxDeltaTime);
+            dt -= maxDeltaTime;
+        }
+        TimeStep(dt);
+    }
+
+    void TimeStep(float dt)
+    {
         Vector3 displacement = anchorPosition - transform.localPosition;
         Vector3 acceleration = springStrength * displacement - dampingStrength * velocity;
-        velocity += acceleration * Time.deltaTime;
-        transform.localPosition += velocity * Time.deltaTime;
+        velocity += acceleration * dt;
+        transform.localPosition += velocity * dt;
     }
 }
